b4da5b1c2da8277cb28fa10bda4d8402684720fb
= <=>
2017-09-14 02:33:57 +0530

    basic design changed

--- question/urls.py
+++ question/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.question_list, name='question_list'),
     url(r'^question/new/$', views.question_new, name='question_new'),
     url(r'^(?P<ques_id>[0-9]+)/$', views.details, name="details"),
-    url(r'^add_ans/$', views.add_ans, name="add_ans")
+    url(r'^add_ans/$', views.add_ans, name="add_ans"),
+    url(r'^add_ques/$', views.add_ques, name="add_ques"),
 ]
--- question/views.py
+++ question/views.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 from django.utils import timezone
 from django.shortcuts import render,redirect, get_object_or_404
 from .models import Question, Answer
-from .forms import QuestionForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
 # Create your views here.
@@ -13,20 +12,19 @@ def question_list(request):
         return render(request,'question/question_list.html',{'questions':questions})
 
 def question_new(request):
-        if request.method=="POST":
-            form=QuestionForm(request.POST)
-            if form.is_valid():
-                question=form.save(commit=False)
-                question.author=request.user
-                question.num_ans=0
-                question.published_date=timezone.now()
-                question.save()
-                url = "/"
-                return HttpResponseRedirect(url)
-                # return redirect('question_detail', pk=question.pk)
-        else:
-            form=QuestionForm()
-        return render(request,'question/question_edit.html',{'form':form})
+        return render(request,'question/question_edit.html',{})
+
+def add_ques(request):
+    question = Question()
+    question.title = request.POST.get("title1","null")
+    question.description = request.POST.get("desc","null")
+    question.author = request.user
+    question.num_ans = 0
+    question.published_date = timezone.now()
+    question.save()
+    url = "/"
+    return HttpResponseRedirect(url)
+
 
 def details(request,ques_id):
     question = get_object_or_404(Question, id=ques_id)
