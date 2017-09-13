75fd3b55738116e954dc55fcfba7d352b9704c45
= <=>
2017-09-13 23:42:06 +0530

    answer addition

--- question/models.py
+++ question/models.py
@@ -6,6 +6,7 @@ class Question(models.Model):
 	title=models.CharField(max_length=200)
 	description=models.TextField()
 	published_date=models.DateTimeField(blank=True,null=True)
+	num_ans = models.IntegerField()
 	#TODO
 	#tags
 	#answers
@@ -15,4 +16,9 @@ class Question(models.Model):
 		self.save()
 
 	def __str__(self):
-		return self.title	
+		return self.title
+
+class Answer(models.Model):
+    ques = models.ForeignKey(Question, on_delete=models.CASCADE)
+    ans_desc = models.TextField(max_length=1000)
+    #ans_date = models.DateTimeField(blank=True,null=True)
--- question/views.py
+++ question/views.py
@@ -1,26 +1,49 @@
-# -*- coding: utf-8 -*-
+    # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.utils import timezone
-from django.shortcuts import render,redirect
-from .models import Question
+from django.shortcuts import render,redirect, get_object_or_404
+from .models import Question, Answer
 from .forms import QuestionForm
-
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import loader
 # Create your views here.
 
 def question_list(request):
-	questions=Question.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request,'question/question_list.html',{'questions':questions})
+        questions=Question.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        return render(request,'question/question_list.html',{'questions':questions})
 
 def question_new(request):
-	if request.method=="POST":
-		form=QuestionForm(request.POST)
-		if form.is_valid():
-			question=form.save(commit=False)
-			question.author=request.user
-			question.published_date=timezone.now()
-			question.save()
-			# return redirect('question_detail', pk=question.pk)
-	else:	
-		form=QuestionForm()
-	return render(request,'question/question_edit.html',{'form':form})
+        if request.method=="POST":
+            form=QuestionForm(request.POST)
+            if form.is_valid():
+                question=form.save(commit=False)
+                question.author=request.user
+                question.num_ans=0
+                question.published_date=timezone.now()
+                question.save()
+                url = "/"
+                return HttpResponseRedirect(url)
+                # return redirect('question_detail', pk=question.pk)
+        else:
+            form=QuestionForm()
+        return render(request,'question/question_edit.html',{'form':form})
 
+def details(request,ques_id):
+    question = get_object_or_404(Question, id=ques_id)
+    return render(request,'question/details.html', {'question':question})
+
+def add_ans(request):
+    ques_id = request.POST["_ques"]
+    ans = request.POST["ans"]
+
+    question = Question.objects.filter(id=ques_id)[0]
+    answer = Answer()
+    answer.ans_desc=ans
+    answer.ques = question
+    answer.save()
+
+    url = "/" + str(ques_id) +"/"
+    return HttpResponseRedirect(url)
+    #context = {'question:question'}
+    #template = loader.get_template("question/details.html")
+    #return HttpResponse(template.render(context, request))
