c78ef3f2caef5e4f5cc9fff5175bea463d8afc35
abhishekarya286 <abhishekarya286@gmail.com>
2017-09-13 21:45:25 +0530

    Added registration form

--- question/views.py
+++ question/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Question
 from .forms import QuestionForm
+
 # Create your views here.
 
 def question_list(request):
@@ -11,6 +12,15 @@ def question_list(request):
 	return render(request,'question/question_list.html',{'questions':questions})
 
 def question_new(request):
-	form=QuestionForm()
+	if request.method=="POST":
+		form=QuestionForm(request.POST)
+		if form.is_valid():
+			question=form.save(commit=False)
+			question.author=request.user
+			question.published_date=timezone.now()
+			question.save()
+			# return redirect('question_detail', pk=question.pk)
+	else:	
+		form=QuestionForm()
 	return render(request,'question/question_edit.html',{'form':form})
 
