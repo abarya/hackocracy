f02c6fa39fd98397da97681d64f2b9802e101772
abhishekarya286 <abhishekarya286@gmail.com>
2017-09-18 13:13:56 +0530

    UI modified

--- question/urls.py
+++ question/urls.py
@@ -6,6 +6,7 @@ app_name="question"
 urlpatterns = [
     url(r'^$', views.question_list, name='question_list'),
     url(r'^question/new/$', views.question_new, name='question_new'),
+    url(r'^register_acc/$', views.UserFormView.as_view(), name='reg'),
     url(r'^(?P<ques_id>[0-9]+)/$', views.details, name="details"),
     url(r'^add_ans/$', views.add_ans, name="add_ans"),
     url(r'^add_ques/$', views.add_ques, name="add_ques"),
--- question/views.py
+++ question/views.py
@@ -4,7 +4,12 @@ from django.utils import timezone
 from django.shortcuts import render,redirect, get_object_or_404
 from .models import Question, Answer
 from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate,login
+from django.views.generic import View
 from django.template import loader
+
+from .forms import UserForm
+
 # Create your views here.
 
 def question_list(request):
@@ -45,3 +50,37 @@ def add_ans(request):
     #context = {'question:question'}
     #template = loader.get_template("question/details.html")
     #return HttpResponse(template.render(context, request))
+
+class UserFormView(View):
+    form_class=UserForm
+    template_name='question/registration_form.html'
+
+    #displays blank form
+    def get(self,request):
+        form=self.form_class(None)
+        return render(request,self.template_name,{'form':form})
+
+    #process from data
+    def post(self,request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            #cleaned or normalized data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            #returns User objects if credentials are correct
+            user = authenticate(username=username,password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request,user)
+                    return redirect(question_list)
+                    
+        return render(request,self.template_name,{'form':form})         
